7b9c9f64315d851f1098715e3991a4d742ac1854
David Kiener <kiener.33@buckeyemail.osu.edu>
2025-08-09 20:05:54 -0400

    initial commit

---
+++ python_repos.py
@@ -0,0 +1,22 @@
+import requests
+
+url="https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept" : "application/vnd.github.v3+json"}
+response = requests.get(url, headers=headers)
+print(f'Status Code: {response.status_code}')
+response_dict = response.json()
+
+print(f"Total Repo: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(response_dict)}")
+
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"\nOwner: {repo_dict['owner']['login']}")
+    print(f"\nStars: {repo_dict['stargazers_count']}")
+    print(f"\nRepository: {repo_dict['html_url']}")
+    print(f"Description: {repo_dict['description']}")
